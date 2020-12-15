25f2d5ce9a1a45949aefb9780f0cd5752a1a23e3
alenahalm <alenahalmatova749@gmail.com>
2020-12-15 21:58:38 +0800

    Initial commit

---
+++ roundedTime.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int h, m, s;
+	cout << "Enter hours, minutes and seconds in one row" << endl;
+	cin >> h >> m >> s;
+	cout << h << "h " << m << "m " << s << "s -> ";
+	cout << h << "h " << m + (s / 30) << "m -> ";
+	cout << h + (m / 30) << "h" << endl;
+	system("pause");
+}
